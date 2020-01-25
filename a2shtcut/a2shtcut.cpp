156c86a2ae4982b93fc26f7e6d23c0065b1b5657
Аlex Kukhtin <alex.kukhtin@outlook.com>
2020-01-25 11:50:42 +0200

    v 7600

--- a2shtcut/a2shtcut.cpp
+++ a2shtcut/a2shtcut.cpp
@@ -9,19 +9,23 @@ class CoInit
 {
 public:
 	CoInit()
-		{ ::CoInitialize(NULL);}
+	{
+		::CoInitialize(NULL);
+	}
 	~CoInit()
-		{ ::CoUninitialize();}
+	{
+		::CoUninitialize();
+	}
 };
 
 // forward
-bool CreateLink(LPCWSTR szTarget, LPCWSTR szPath, LPCWSTR szName, LPCWSTR szWDir, int nIcon, LPCWSTR szDescr, LPCWSTR szArgs);
+bool CreateLink(LPCWSTR szTarget, LPCWSTR szPath, LPCWSTR szName, LPCWSTR szWDir, int nIcon, LPCWSTR szIconPath, LPCWSTR szDescr, LPCWSTR szArgs);
 void RemoveLink(LPCWSTR szTarget, LPCWSTR szName);
 
 int APIENTRY _tWinMain(HINSTANCE hInstance,
-                     HINSTANCE hPrevInstance,
-                     LPTSTR    lpCmdLine,
-                     int       nCmdShow)
+	HINSTANCE hPrevInstance,
+	LPTSTR    lpCmdLine,
+	int       nCmdShow)
 {
 	CoInit ci;
 	enum CMD {
@@ -38,6 +42,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 	CString name;   // link name
 	CString wdir;   // working dir
 	int     icon = -1;
+	CString idir; // icon directory
 	CString descr; // description
 	CString args;  // path arguments
 
@@ -48,51 +53,63 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 		int len = token.GetLength();
 		if (token == L"add") {
 			cmd = _cmd_add;
-		} else if (token == L"remove") {
+		}
+		else if (token == L"remove") {
 			cmd = _cmd_remove;
-		} else if (token.Find(L"target=") == 0) {
+		}
+		else if (token.Find(L"target=") == 0) {
 			target = token.Right(len - 7);
-		}	else if (token.Find(L"path=") == 0) {
+		}
+		else if (token.Find(L"path=") == 0) {
 			path = token.Right(len - 5);
-		} else if (token.Find(L"name=") == 0) {
+		}
+		else if (token.Find(L"name=") == 0) {
 			name = token.Right(len - 5);
-		} else if (token.Find(L"wdir=") == 0) {
+		}
+		else if (token.Find(L"wdir=") == 0) {
 			wdir = token.Right(len - 5);
-		} else if (token.Find(L"icon=") == 0) {
+		}
+		else if (token.Find(L"idir=") == 0) {
+			idir = token.Right(len - 5);
+		}
+		else if (token.Find(L"icon=") == 0) {
 			icon = _ttoi(token.Right(len - 5));
-		} else if (token.Find(L"descr=") == 0) {
+		}
+		else if (token.Find(L"descr=") == 0) {
 			descr = token.Right(len - 6);
-		} else if (token.Find(L"args=") == 0) {
+		}
+		else if (token.Find(L"args=") == 0) {
 			args = token.Right(len - 5);
 		}
 		token = str.Tokenize(L"/", pos);
 	}
-	
+
 	if (cmd == _cmd_add) {
-		if (!CreateLink(target, path, name, wdir, icon, descr, args)) {
+		if (!CreateLink(target, path, name, wdir, icon, idir, descr, args)) {
 			CString str;
-			str.Format(L"Error creating shortcut.\ntarget='%s'\npath='%s'\nname='%s'", (LPCWSTR) target, (LPCWSTR) path, (LPCWSTR)name);
-			::MessageBox(NULL, str, L"A2v10", MB_OK|MB_ICONEXCLAMATION);
+			str.Format(L"Error creating shortcut.\ntarget='%s'\npath='%s'\nname='%s'", (LPCWSTR)target, (LPCWSTR)path, (LPCWSTR)name);
+			::MessageBox(NULL, str, L"A2v10", MB_OK | MB_ICONEXCLAMATION);
 		}
-	} else if (cmd == _cmd_remove) {
+	}
+	else if (cmd == _cmd_remove) {
 		RemoveLink(target, name);
 	}
 	else {
-		::MessageBox(nullptr, L"usage a2shtcut -[add!remove] [params]", L"A2v10", MB_OK|MB_ICONEXCLAMATION);
+		::MessageBox(nullptr, L"usage a2shtcut -[add!remove] [params]", L"A2v10", MB_OK | MB_ICONEXCLAMATION);
 	}
 
 	return 0;
 }
 
-bool CreateLink(LPCWSTR szTarget, LPCWSTR szPath, LPCWSTR szName, LPCWSTR szWDir, int nIcon, LPCWSTR szDescr, LPCWSTR szArgs)
+bool CreateLink(LPCWSTR szTarget, LPCWSTR szPath, LPCWSTR szName, LPCWSTR szWDir, int nIcon, LPCWSTR szIconPath, LPCWSTR szDescr, LPCWSTR szArgs)
 {
-	CComPtr<IShellLinkW> pShellLink;  
+	CComPtr<IShellLinkW> pShellLink;
 	HRESULT hr = S_OK;
-	hr = CoCreateInstance(CLSID_ShellLink, NULL, CLSCTX_INPROC_SERVER, 
-		IID_IShellLinkW, 
+	hr = CoCreateInstance(CLSID_ShellLink, NULL, CLSCTX_INPROC_SERVER,
+		IID_IShellLinkW,
 		reinterpret_cast<void **>(&pShellLink));
 	if (FAILED(hr))
-	 return false;
+		return false;
 	CComQIPtr<IPersistFile> pFile = pShellLink;
 	if (pFile == NULL)
 		return false;
@@ -101,16 +118,19 @@ bool CreateLink(LPCWSTR szTarget, LPCWSTR szPath, LPCWSTR szName, LPCWSTR szWDir
 	if (szArgs && *szArgs) {
 		pShellLink->SetArguments(szArgs);
 	}
-	if (nIcon != -1)
+	if (szIconPath)
+		pShellLink->SetIconLocation(szIconPath, 0);
+	else if (nIcon != -1)
 		pShellLink->SetIconLocation(szPath, nIcon);
+
 	pShellLink->SetWorkingDirectory(szWDir);
 	pShellLink->SetDescription(szDescr);
 
-	WCHAR linkName[MAX_PATH + 1] = {0};
+	wchar_t linkName[MAX_PATH + 1] = { 0 };
 	wcscpy_s(linkName, MAX_PATH, szName);
 	wcscat_s(linkName, MAX_PATH, L".lnk");
 
-	WCHAR path[MAX_PATH + 1];
+	wchar_t path[MAX_PATH + 1];
 	wcscpy_s(path, MAX_PATH, szTarget);
 	::PathAppend(path, linkName);
 	hr = pFile->Save(path, TRUE);
@@ -122,11 +142,11 @@ bool CreateLink(LPCWSTR szTarget, LPCWSTR szPath, LPCWSTR szName, LPCWSTR szWDir
 void RemoveLink(LPCWSTR szTarget, LPCWSTR szName)
 {
 
-	WCHAR linkName[MAX_PATH] = {0};
+	wchar_t linkName[MAX_PATH + 1] = { 0 };
 	wcscpy_s(linkName, MAX_PATH, szName);
 	wcscat_s(linkName, MAX_PATH, L".lnk");
 
-	WCHAR path[MAX_PATH + 1];
+	wchar_t path[MAX_PATH + 1];
 	wcscpy_s(path, MAX_PATH, szTarget);
 
 
